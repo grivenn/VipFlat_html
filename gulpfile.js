b1d0142b86178fa15657f7d79035cc186741c3df
DESKTOP-NN816ET\ogriv <ogriven@gmail.com>
2025-09-04 09:50:33 +0300

    Добавлено оглавление. index.html разделен на вложеные файлы gulp настроен на вывод в папку dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
-var gulp           = require('gulp'),
-		sass           = require('gulp-sass')(require('sass')),
-		browserSync    = require('browser-sync'),
-		concat         = require('gulp-concat'),
-		uglify         = require('gulp-uglify'),
-		cleanCSS       = require('gulp-clean-css'),
-		rename         = require('gulp-rename'),
-		autoprefixer   = require('gulp-autoprefixer'),
-		notify         = require("gulp-notify");
+const 	gulp			= require('gulp'),
+		sass			= require('gulp-sass')(require('sass')),
+		browserSync		= require('browser-sync'),
+		concat			= require('gulp-concat'),
+		uglify			= require('gulp-uglify'),
+		cleanCSS		= require('gulp-clean-css'),
+		rename			= require('gulp-rename'),
+		autoprefixer	= require('gulp-autoprefixer'),
+		notify			= require("gulp-notify"),
+		fileInclude		= require('gulp-file-include');
+
 
 // Сервер и автообновление страницы Browsersync
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: 'dist'
+			// baseDir: 'app'
 		},
 		notify: false,
 		// tunnel: true,
@@ -24,11 +27,11 @@ gulp.task('browser-sync', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		// 'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Всегда в конце
+		// 'app/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Минимизировать весь js (на выбор)
-	.pipe(gulp.dest('app/js'))
+	.pipe(gulp.dest('dist/js'))
 	.pipe(browserSync.reload({ stream: true}));
 });
 
@@ -39,25 +42,31 @@ gulp.task('sass', function() {
 	// .pipe(rename({suffix: '.min', prefix : ''}))
 	// .pipe(autoprefixer(['last 15 versions']))
 	// .pipe(cleanCSS()) // Опционально, закомментировать при отладке
-	.pipe(gulp.dest('app/Styles'))
+	.pipe(gulp.dest('dist/Styles'))
 	.pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('css', function() {
-	return gulp.src('app/Styles/*.css')
-	.pipe(browserSync.reload({ stream: true }))
-});
+// gulp.task('css', function() {
+// 	return gulp.src('app/Styles/*.css')
+// 	.pipe(browserSync.reload({ stream: true }))
+// });
 
 gulp.task('code', function() {
-	return gulp.src('app/**/*.html')
+	// return gulp.src('app/**/*.html')
+	return gulp.src('app/index.html')
+	.pipe(fileInclude({
+		prefix: '@@',    // Префикс для вставки
+		basepath: '@file' // Путь к файлам
+	}))
+	.pipe(gulp.dest('dist'))
 	.pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('watch', function() {
 	gulp.watch('app/sass/*.sass', gulp.parallel('sass'));
 	// gulp.watch('app/Styles/**/*.css', gulp.parallel('css'));
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('js'));
-	gulp.watch('app/*.html', gulp.parallel('code'));
+	// gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('js'));
+	gulp.watch(['app/*.html', 'app/partials/*.html'], gulp.parallel('code'));
 });
 
-gulp.task('default', gulp.parallel('sass', 'js', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
